17e74f18a95b75065eade4169213b0231f11d91e
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-23 11:50:17 +0300

    added Header.tsx Componenet

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { NavLink } from '@/interfaces';
+
+function Header() {
+    const router = useRouter();
+    const isActive = (href: string) =>
+        router.pathname === href ? 'text-blue-600 font-semibold ' : 'text-gray-300 hover:text-blue-600 hover:shadow-md px-4 py-2 hover:bg-white rounded';
+
+    const linksArray: NavLink[] = [
+        { name: 'Home', href: '/' },
+        { name: 'Properties', href: '/properties' },
+        { name: 'About', href: '/about' },
+        { name: 'Contact us', href: '/contact' },
+    ];
+
+    return (
+        <header className="bg-gray-900 shadow-md sticky top-0 z-50">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
+
+                <Link href="/" className="text-white text-xl font-bold tracking-tight">
+                    MySite
+                </Link>
+
+                {/* Navigation Links */}
+                <nav aria-label="Main Navigation">
+                    <ul className="flex space-x-6 items-center">
+                        {linksArray.map((navItem) => (
+                            <li key={navItem.name}>
+                                <Link
+                                    href={navItem.href}
+                                    className={`transition-colors duration-200 ${isActive(navItem.href)}`}
+                                >
+                                    {navItem.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '@/components/layout/Header'
 
 function index() {
   return (
     <div>
+      <Header />
       <h1 className='text-4xl font-bold text-center mt-[calc(50vh-50px)]  p-4'>Welcome to My Website</h1>
       <p className='text-lg text-center mt-2 p-4'>This is the homepage.</p>
 
