2ad06ddf3ba5cb951b2a3e42e191cbc0b562cfb5
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-24 12:37:36 +0300

    added Users page

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,4 +1,4 @@
-import { type GetStaticProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import UserCard from '@/components/common/UserCard';
 import { type UserProps } from '@/interfaces';
